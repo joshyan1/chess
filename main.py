9e038b99f79d070222548fac983d1d99f26e6bd0
josh <jyan00017@gmail.com>
2023-07-30 17:32:00 -0700

    hellO!

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
 from flask import Flask, request
 import requests
+from redislite import Redis
+import msgpack
 
-app = Flask(__name__)
+app = Flask(__name__) 
+redis = Redis("chess.db") #creates redis database
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/search", methods=["POST", "GET"])
+@app.route("/search", methods=["GET"])
 def search():
-    username = request.form.get('username', 'thebigjoshua')
-    data = requests.get(f"https://api.chess.com/pub/player/{username}")
-    data.raise_for_status()
-    
-    return data.json()["username"]
+
+    #sets username to query data or default (thebigjoshua)
+    #uses query parameters (localhost:5000?username=thebigjoshua&site=chess)
+    username = request.args.get('username', 'thebigjoshua')
+    site = request.args.get('site', 'chess')
+
+    #check if account exists
+    if redis.exists(f'{site}:{username}'):
+        cache = msgpack.unpack(redis.get(f'{site}:{username}')) #unpacks data from binary to string
+        print(cache)
+        return cache
+
+    print(f'database doesnt have {site}:{username}')
+
+    #pulls from site
+    if site == 'chess':
+        data = requests.get(f"https://api.chess.com/pub/player/{username}")
+    elif site == 'lichess':
+        data = requests.get(f"https://lichess.org/api/user/{username}")
+    else:
+        raise Exception("site not accepted")
+
+    #validation
+    data.raise_for_status() 
+    data2 = msgpack.packb(data.json()) #packs data to binary
+
+    redis.set(f'{site}:{username}', data2) #adds to database
+    return msgpack.unpackb(data2) #unpacks
+
+    '''
+    website: search 'username'
+
+    get data from lichess, chess.com, uscf, etc.
+
+    return: lichess acc, chess.com acc, uscf acc
+
+    user selects accounts
+    '''
