0e582ad5e735359d765284239c177207540e8019
josh <jyan00017@gmail.com>
2023-08-13 23:53:20 -0700

    comments + use in readme

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ poetry install
 poetry run flask --app main run
 ```
 
+# use
+```
+go to 127.0.0.1:5000/search?username=anyusername&site=chess
+should display the json game data for the first game from that account
+```
--- main.py
+++ main.py
@@ -7,35 +7,45 @@ import time
 app = Flask(__name__) 
 redis = Redis("chess.db") #creates redis database
 
+#classes for abstraction business --> look below at function calls, pretty easier
+#class for chess.com profiles
 class Chesscom:
     def __init__(self, username):
         self.username = username
 
+    #gets profile
     def profile(self):
         response = requests.get(f"https://api.chess.com/pub/player/{self.username}")
         response.raise_for_status()
 
         return response.json()    
 
+    #gets stats
     def stats(self):
         response = requests.get(f"https://api.chess.com/pub/player/{self.username}/stats")
         response.raise_for_status()
 
         return response.json()    
     
+    #gets list of urls to monthly archives of games and adds individual games to list
     def games(self):
         response = requests.get(f"https://api.chess.com/pub/player/{self.username}/games/archives")
         response.raise_for_status()
 
+        #creates list
         games = []
 
         for archive in response.json().get('archives'):
+
+            #uses monthly archive url to get list of games
             response2 = requests.get(archive)
             response2.raise_for_status()
+            #adds eaech game to list
             games.extend(response2.json().get("games"))
 
         return games
 
+#class for lichess profiles
 class Lichess:
     def __init__(self, username):
         self.username = username
@@ -66,6 +76,7 @@ def search():
         print(cache)
         #return cache
 
+    #disregard for now
     print(f'database doesnt have {site}:{username}')
 
     #pulls from site
@@ -76,19 +87,29 @@ def search():
     else:
         raise Exception("site not accepted")
 
-    #validation 
+    #pulls data 
     profile = user.profile()
     games = user.games()
     stats = user.stats()
 
     #adds packed profile to databse
     redis.set(f'{site}:{username}', msgpack.packb(profile)) 
+
+    #runs through list of games
     for game in games:
-        print(game.keys())
-        print(type(game.get("end_time")))
+        #print(game.keys())
+        #print(type(game.get("end_time")))
+
+        #adds each game to redis database's sorted list
+        #first paramater is database name
+        #second parameter is dict: (key:score)
+            #key is game data, score is timestamp of end time so we can find games by time
         redis.zadd(f'{site}:{username}:games', {msgpack.packb(game):float(game.get("end_time"))})
+
+    #adds packed stats to database
     redis.set(f'{site}:{username}:stats', msgpack.packb(stats))
 
+    #just returns first game in games database to site
     return msgpack.unpackb(redis.zrange(f'{site}:{username}:games', "0", time.time(), byscore=True)[0])
  
 
